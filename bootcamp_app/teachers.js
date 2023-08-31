cd821d6a9556b4203c03132f30f4f33eb9c62b86
MrMoon127 <sasha.andia@gmail.com>
2023-08-30 19:03:39 -0700

    did some database input sanitization to avoid malicious inputs

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT cohorts.name as cohort, teachers.name as teacher
 FROM cohorts
 JOIN students ON cohorts.id = students.cohort_id
 JOIN assistance_requests ON students.id = assistance_requests.student_id
 JOIN teachers ON teachers.id = assistance_requests.teacher_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name LIKE $1
 GROUP BY teachers.name, cohorts.name;
-`)
+`;
+
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
